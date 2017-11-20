2b9c9b8100a99ec501bd00f604f073eedc7e1d3c
luccafort <muspellsheim1985@gmail.com>
2017-11-21 07:39:38 +0900

    ニュースカードの表示を改善
    ニュースカードの表示を4つ毎にまとめるコードを実装
    取得したニュース情報からトップに表示する画像情報を設定する実装を追加

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Domain  = 'api.nytimes.com';
 const BasePath = '/svc/topstories/v2/';
-const API_KEY = 'YOUR-API-KEY';
+const API_KEY = 'a409a682764b45a0aaa15393c66bf8ca';
 
 function buildUrl(url) {
   return 'https://' + Domain + BasePath + url + '.json?api_key=' + API_KEY;
@@ -11,7 +11,7 @@ const vm = new Vue({
     data: {
         results: []
     },
-    mounted(){
+    mounted() {
         this.getPost('home');
     },
     methods: {
@@ -20,6 +20,24 @@ const vm = new Vue({
         axios.get(url).then((response) => {
           this.results = response.data.results;
         }).catch(error => { console.log(error); });
+      }
+    },
+    computed: {
+      // resultsが変更されるたびに呼び出されて画像の取得とResultsを4つにまとめる処理が実行される
+      processPost() {
+        let post = this.results
+        post.map(post => {
+          let image = post.multimedia.find(media => media.format === 'superJumbo');
+          post.image_url = image ? image.url : 'http://placehold.it/300x200?text=N/A';
+        });
+
+        let i,j;
+        let chunkedArray = [];
+        let chunk = 4;
+        for(i=0, j=0; i < post.length; i += chunk, j++) {
+          chunkedArray[j] = post.slice(i, i+chunk);
+        }
+        return chunkedArray;
       }
     }
 });

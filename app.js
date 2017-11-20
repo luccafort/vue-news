6223ac82379936a3577af9ec4a5403ce003aa03f
luccafort <muspellsheim1985@gmail.com>
2017-11-20 23:43:59 +0900

    コードの再利用性を高めた実装に修正
    URLを生成する関数を新規追加
    マウント時にVueインスタンスメソッドを参照するよう変更
    MethodsオブジェクトにgetPost()関数を追加、非同期通信処理をgetPost()内で行うよう修正

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
+const Domain  = 'api.nytimes.com';
+const BasePath = '/svc/topstories/v2/';
+const API_KEY = 'YOUR-API-KEY';
+
+function buildUrl(url) {
+  return 'https://' + Domain + BasePath + url + '.json?api_key=' + API_KEY;
+}
+
 const vm = new Vue({
     el: '#app',
     data: {
         results: []
     },
     mounted(){
-        axios.get("https://api.nytimes.com/svc/topstories/v2/home.json?api-key=YOUR-API-KEY")
-        .then(response => {this.results = response.data.results})
+        this.getPost('home');
+    },
+    methods: {
+      getPost(section) {
+        let url = buildUrl(section);
+        axios.get(url).then((response) => {
+          this.results = response.data.results;
+        }).catch(error => { console.log(error); });
+      }
     }
 });
